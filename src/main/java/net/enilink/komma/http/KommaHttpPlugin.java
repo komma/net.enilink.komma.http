8e3abe58d15a671ec8fef6c674e25c63f3650a8d
Marko Friedemann <marko.friedemann@iwu.fraunhofer.de>
2017-11-21 13:08:27 +0100

    Mark fixed strings as non-localized. Add method to get relative URL. Wrap Request.
    Mark all fixed strings as NON-NLS-x to ignore in eclipse translation tooling.
    Add method to get server-root-relative URL path for resource.
    Wrap Request in ContentServlet to drop query parameters etc. to avoid them being
      used as part of the resource path.

--- src/main/java/net/enilink/komma/http/KommaHttpPlugin.java
+++ src/main/java/net/enilink/komma/http/KommaHttpPlugin.java
@@ -21,24 +21,29 @@ import org.osgi.util.tracker.ServiceTracker;
 import net.enilink.komma.http.servlet.ContentServlet;
 
 public class KommaHttpPlugin extends Plugin {
+
+	public static final String PLUGIN_ID = "net.enilink.komma.http"; //$NON-NLS-1$
+
 	protected final static String environmentKeyHttpPort = "org.osgi.service.http.port"; //$NON-NLS-1$
 
+	protected final static String serviceKeyHttpScheme = "http.scheme"; //$NON-NLS-1$
 	protected final static String serviceKeyHttpAddress = "http.address"; //$NON-NLS-1$
 	protected final static String serviceKeyHttpPort = "http.port"; //$NON-NLS-1$
-	protected final static String serviceKeyHttpScheme = "http.scheme"; //$NON-NLS-1$
+
+	protected static final String URL_PATH = "/eclipse-resources"; //$NON-NLS-1$
 
 	private static KommaHttpPlugin instance;
 
-	public static final String PLUGIN_ID = "net.enilink.komma.http";
-
 	ServiceTracker<HttpService, HttpService> defaultHttpServiceTracker;
 	String httpServiceUrl;
+	boolean registered;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
 		instance = this;
 		super.start(context);
 
+		registered = false;
 		defaultHttpServiceTracker = installDefaultHttpServiceTracker(context);
 
 		// try to start some OSGi HTTP Service if not already running
@@ -47,23 +52,23 @@ public class KommaHttpPlugin extends Plugin {
 
 			// Jetty is not available or could not be started,
 			// try to use PAX Web or Equinox HTTP service
-			for (String bundleName : Arrays.asList(
-					"org.eclipse.equinox.http.jetty",
-					"org.ops4j.pax.web.pax-web-jetty-bundle",
-					"org.ops4j.pax.web.pax-web-jetty",
-					"org.eclipse.equinox.http")) {
+			for (String bundleName : Arrays.asList( //
+					"org.eclipse.equinox.http.jetty", //$NON-NLS-1$
+					"org.ops4j.pax.web.pax-web-jetty-bundle", //$NON-NLS-1$
+					"org.ops4j.pax.web.pax-web-jetty", //$NON-NLS-1$
+					"org.eclipse.equinox.http" //$NON-NLS-1$
+			)) {
 				Bundle httpBundle = Platform.getBundle(bundleName);
 				if (httpBundle != null) {
 					// allow automatic selection of free port
 					if (port == null) {
-						System.setProperty(environmentKeyHttpPort, "0");
+						System.setProperty(environmentKeyHttpPort, "0"); //$NON-NLS-1$
 					}
 					try {
 						httpBundle.start(Bundle.START_TRANSIENT);
 						return;
 					} catch (Exception e2) {
-						logError("Unable to start HTTP server bundle: "
-								+ bundleName, e2);
+						logError("Unable to start HTTP server bundle: " + bundleName, e2);
 					}
 				}
 			}
@@ -76,7 +81,7 @@ public class KommaHttpPlugin extends Plugin {
 			defaultHttpServiceTracker.close();
 			defaultHttpServiceTracker = null;
 		}
-
+		registered = false;
 		super.stop(context);
 		instance = null;
 	}
@@ -85,69 +90,61 @@ public class KommaHttpPlugin extends Plugin {
 		return instance;
 	}
 
+	public boolean isRegistered() {
+		return registered;
+	}
+
 	/**
 	 * Logs an Error message with an exception.
 	 */
 	public static synchronized void logError(String message, Throwable ex) {
 		if (message == null)
 			message = ""; //$NON-NLS-1$
-		Status errorStatus = new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK,
-				message, ex);
+		Status errorStatus = new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK, message, ex);
 		KommaHttpPlugin.getDefault().getLog().log(errorStatus);
 	}
 
-	ServiceTracker<HttpService, HttpService> installDefaultHttpServiceTracker(
-			BundleContext context) throws Exception {
-		String filterString = "(" + Constants.OBJECTCLASS + "="
-				+ HttpService.class.getName() + ")";
+	ServiceTracker<HttpService, HttpService> installDefaultHttpServiceTracker(BundleContext context) throws Exception {
+		String filterString = "(" + Constants.OBJECTCLASS + "=" + HttpService.class.getName() + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 		Filter filter = context.createFilter(filterString);
-		ServiceTracker<HttpService, HttpService> defaultHttpServiceTracker = new ServiceTracker<HttpService, HttpService>(
-				context, filter, null) {
+		ServiceTracker<HttpService, HttpService> defaultHttpServiceTracker = new ServiceTracker<HttpService, HttpService>(context, filter, null) {
 			@Override
-			public HttpService addingService(
-					ServiceReference<HttpService> reference) {
-				String scheme = (String) reference
-						.getProperty(serviceKeyHttpScheme);
+			public HttpService addingService(ServiceReference<HttpService> reference) {
+				String scheme = (String) reference.getProperty(serviceKeyHttpScheme);
 				if (scheme == null) {
-					scheme = "http";
+					scheme = "http"; //$NON-NLS-1$
 				}
-				String address = (String) reference
-						.getProperty(serviceKeyHttpAddress);
-				if (address == null || "ALL".equals(address.toUpperCase())) {
-					address = "127.0.0.1";
+				String address = (String) reference.getProperty(serviceKeyHttpAddress);
+				if (address == null || "ALL".equals(address.toUpperCase())) { //$NON-NLS-1$
+					address = "127.0.0.1"; //$NON-NLS-1$
 				}
 				Object portValue = reference.getProperty(serviceKeyHttpPort);
 				if (portValue == null) {
 					portValue = reference.getProperty(environmentKeyHttpPort);
 				}
-				int port = portValue != null ? Integer.valueOf(portValue
-						.toString()) : 0;
+				int port = portValue != null ? Integer.valueOf(portValue.toString()) : 0;
 
-				String serviceUrl = scheme + "://" + address + ":" + port;
+				String serviceUrl = scheme + "://" + address + ":" + port; //$NON-NLS-1$ //$NON-NLS-2$
 				System.out.println("HTTP Service: " + serviceUrl);
 
-				// TODO find a better way to determine if we are running in a
-				// server environment
+				// TODO find a better way to determine if we are running in a server environment
 				boolean isRapRunning = false;
 				try {
-					Bundle rapBundle = Platform.getBundle("org.eclipse.rap.ui");
-					isRapRunning = rapBundle != null
-							&& (rapBundle.getState() & (Bundle.ACTIVE
-									| Bundle.STARTING | Bundle.RESOLVED)) != 0;
+					Bundle rapBundle = Platform.getBundle("org.eclipse.rap.ui"); //$NON-NLS-1$
+					isRapRunning = rapBundle != null && (rapBundle.getState() & (Bundle.ACTIVE | Bundle.STARTING | Bundle.RESOLVED)) != 0;
 				} catch (Throwable exception) {
 					// Assume that it's not available.
 				}
 				if (isRapRunning) {
-					// simply use absolute paths for serving resources when
-					// running in a web context
-					httpServiceUrl = "";
+					// simply use absolute paths for serving resources when running in a web context
+					httpServiceUrl = ""; //$NON-NLS-1$
 				} else {
 					httpServiceUrl = serviceUrl;
 				}
 
 				HttpService service = super.addingService(reference);
-				registerResources(service);
+				registered = registerResources(service);
 				return service;
 			}
 		};
@@ -156,12 +153,11 @@ public class KommaHttpPlugin extends Plugin {
 		return defaultHttpServiceTracker;
 	}
 
-	void registerResources(HttpService httpService) {
+	boolean registerResources(HttpService httpService) {
 		// register servlets and resources for HttpService
 		try {
-			httpService.registerServlet("/eclipse-resources",
-					new ContentServlet(), new Hashtable<String, String>(),
-					httpService.createDefaultHttpContext());
+			httpService.registerServlet(URL_PATH, new ContentServlet(), new Hashtable<String, String>(), httpService.createDefaultHttpContext());
+			return true;
 		} catch (ServletException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -169,17 +165,22 @@ public class KommaHttpPlugin extends Plugin {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	public static String createHttpUrl(String resourceUrl) {
-		String httpServiceUrl = getDefault().httpServiceUrl;
-		if (httpServiceUrl != null) {
-			return httpServiceUrl + "/eclipse-resources/" + resourceUrl;
-		}
-		return resourceUrl;
+		return false;
 	}
 
 	public String getServiceUrl() {
 		return httpServiceUrl;
+	}
+
+	public static String createHttpUrl(String resourceUrl) {
+		String httpServiceUrl = getDefault().getServiceUrl();
+		if (httpServiceUrl != null) {
+			return httpServiceUrl + createUrlPath(resourceUrl);
+		}
+		return resourceUrl;
+	}
+
+	public static String createUrlPath(String resourceUrl) {
+		return URL_PATH + "/" + resourceUrl; //$NON-NLS-1$
 	}
 }
--- src/main/java/net/enilink/komma/http/servlet/ContentServlet.java
+++ src/main/java/net/enilink/komma/http/servlet/ContentServlet.java
@@ -11,10 +11,14 @@
 package net.enilink.komma.http.servlet;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -35,31 +39,44 @@ public class ContentServlet extends HttpServlet {
 	}
 
 	/**
-	 * Called by the server (via the <code>service</code> method) to allow a
-	 * servlet to handle a GET request.
+	 * Called by the server (via the <code>service</code> method) to allow a servlet to handle a GET request.
 	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// required for "org.eclipse.equinox.http" service
-		if (getServletContext().getMajorVersion() > 2
-				|| getServletContext().getMinorVersion() >= 4) {
+		if (getServletContext().getMajorVersion() > 2 || getServletContext().getMinorVersion() >= 4) {
 			req.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
 		}
+		// do wrap the request into one without query string and parameters
+		// otherwise it might try to get a resource with it appended
+		HttpServletRequestWrapper wrappedReq = new HttpServletRequestWrapper(req) {
+			@Override
+			public String getQueryString() {
+				return null;
+			}
+
+			@Override
+			public Enumeration<String> getParameterNames() {
+				return Collections.emptyEnumeration();
+			}
+
+			@Override
+			public Map<String, String[]> getParameterMap() {
+				return Collections.emptyMap();
+			}
+		};
 		if (connector != null) {
-			connector.transfer(req, resp);
+			connector.transfer(wrappedReq, resp);
 		}
 	}
 
 	/**
 	 * 
-	 * Called by the server (via the <code>service</code> method) to allow a
-	 * servlet to handle a POST request.
+	 * Called by the server (via the <code>service</code> method) to allow a servlet to handle a POST request.
 	 * 
 	 * Handle the search requests,
 	 * 
 	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		if (connector != null)
 			connector.transfer(req, resp);
 	}
